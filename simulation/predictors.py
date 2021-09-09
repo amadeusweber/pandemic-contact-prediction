c2a319d2413d1526884407c80af45eb90173d153
Amadeus <>
2021-09-09 19:56:24 +0200

    added maximum total predictions

--- simulation/predictors.py
+++ simulation/predictors.py
@@ -15,37 +15,38 @@ def no_agents(c, a):
 def remembered_agents(c, a):
     return c
 
+
 def undirected(c, a):
     return np.logical_or(c, c.T)
 
 
-def create_jaccard(min_threshold=0, max_fraction=1.0):
+def create_jaccard(min_threshold=0, max_fraction=1.0, max_total=float('inf')):
     def p(c, a): return pf.modular_predictor(
-        c, a, nx.jaccard_coefficient, min_threshold, max_fraction)
+        c, a, nx.jaccard_coefficient, min_threshold, max_fraction, max_total)
     return p
 
 
-def create_common_neighbors(min_threshold=0, max_fraction=1.0):
+def create_common_neighbors(min_threshold=0, max_fraction=1.0, max_total=float('inf')):
     def p(c, a): return pf.modular_predictor(
-        c, a, pf.common_neighbors_lp, min_threshold, max_fraction)
+        c, a, pf.common_neighbors_lp, min_threshold, max_fraction, max_total)
     return p
 
 
-def create_adamic_adar(min_threshold=0, max_fraction=1.0):
+def create_adamic_adar(min_threshold=0, max_fraction=1.0, max_total=float('inf')):
     def p(c, a): return pf.modular_predictor(
-        c, a, nx.adamic_adar_index, min_threshold, max_fraction)
+        c, a, nx.adamic_adar_index, min_threshold, max_fraction, max_total)
     return p
 
 
-def create_preferential_attachment(min_threshold=0, max_fraction=1.0):
+def create_preferential_attachment(min_threshold=0, max_fraction=1.0, max_total=float('inf')):
     def p(c, a): return pf.modular_predictor(
-        c, a, nx.preferential_attachment, min_threshold, max_fraction)
+        c, a, nx.preferential_attachment, min_threshold, max_fraction, max_total)
     return p
 
 
-def create_katz(beta=0.1, max_l=None, min_threshold=0, max_fraction=1.0):
+def create_katz(beta=0.1, max_l=None, min_threshold=0, max_fraction=1.0, max_total=float('inf')):
     def predictor(G): return pf.katz_lp(G, beta=beta, max_l=max_l)
 
     def p(c, a): return pf.modular_predictor(
-        c, a, predictor, min_threshold, max_fraction)
+        c, a, predictor, min_threshold, max_fraction, max_total)
     return p
--- simulation/predictors_functions.py
+++ simulation/predictors_functions.py
@@ -26,7 +26,7 @@ def get_unconnected_nodes(G):
     return result
 
 
-def modular_predictor(c, a, predictor, min_threshold=0, max_fraction=1.0):
+def modular_predictor(c, a, predictor, min_threshold=0, max_fraction=1.0, max_total=float("inf")):
     G = nx.from_numpy_matrix(c).to_undirected()
     result = nx.to_numpy_array(G, dtype=bool)
 
@@ -36,6 +36,7 @@ def modular_predictor(c, a, predictor, min_threshold=0, max_fraction=1.0):
     scores.sort(key=lambda x: x[2], reverse=True)
 
     limit = int(max_fraction * len(scores))
+    limit = min(limit, max_total)
     for i in range(limit):
         u, v, p = scores[i]
         if p < min_threshold:
@@ -54,7 +55,8 @@ def common_neighbors_lp(G):
 
 def katz_lp(G, beta=0.1, max_l=None):
     for u, v in get_unconnected_nodes(G):
-        all_path_num = count([len(p) for p in nx.all_simple_paths(G, u, v, max_l)])
+        all_path_num = count([len(p)
+                             for p in nx.all_simple_paths(G, u, v, max_l)])
         katz = 0.0
         for l, p in enumerate(all_path_num):
             katz += (beta ** l) + p
